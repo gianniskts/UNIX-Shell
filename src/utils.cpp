46479fe6c47d77088c5fa200594f0705b88e76e1
gianniskts <johnkotsias@hotmail.gr>
2023-04-13 16:17:07 +0300

    d

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,15 +6,44 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <glob.h>
 #include "../include/utils.h"
 
-void parseCommand(char** tokens, char* users_command) {
-     // Parse the input command string into a vector of tokens (arguments).
-        char* token = strtok(users_command, " \n");
-        int i = 0;
-        while (token != NULL) {
-            tokens[i++] = token;
-            token = strtok(NULL, " \n");
+// void parseCommand(char** tokens, char* users_command) {
+//      // Parse the input command string into a vector of tokens (arguments).
+//         char* token = strtok(users_command, " \n");
+//         int i = 0;
+//         while (token != NULL) {
+//             tokens[i++] = token;
+//             token = strtok(NULL, " \n");
+//         }
+//         tokens[i] = NULL;
+// } 
+
+void parseCommand(char** tokens, char* command) {
+    char* token = strtok(command, " \n"); // tokenize the command string
+
+    int i = 0;
+    while (token != NULL) { // loop through each token in the command
+        glob_t paths; // create a glob_t structure to store the matched paths
+        int flags = 0;
+
+        if (strchr(token, '*') != NULL || strchr(token, '?') != NULL) { // check if the token contains wildcards
+            flags |= GLOB_TILDE; // set the GLOB_TILDE flag to expand tilde characters
+            glob(token, flags, NULL, &paths); // expand the wildcard pattern using glob
+
+            // loop through the matching paths and add them to the tokens array
+            for (int j = 0; j < paths.gl_pathc; j++) {
+                tokens[i++] = strdup(paths.gl_pathv[j]);
+            }
+
+            globfree(&paths); // free the memory allocated by glob
+        } else {
+            tokens[i++] = strdup(token); // add the token to the tokens array as-is
         }
-        tokens[i] = NULL;
-} 
+
+        token = strtok(NULL, " \n"); // move to the next token
+    }
+
+    tokens[i] = NULL; // add a NULL terminator to the end of the tokens array
+}
