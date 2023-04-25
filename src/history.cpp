96b817d59f71230178dfa3c507f092e1ce56499f
gianniskts <johnkotsias@hotmail.gr>
2023-04-25 18:32:31 +0300

    d

--- src/history.cpp
+++ src/history.cpp
@@ -15,16 +15,16 @@
 using namespace std;
 
 void addHistory(char* command, char* history[], int* history_index) {
-    history[*history_index % HISTORY_SIZE] = strdup(command);
-    (*history_index)++;
+    history[*history_index % HISTORY_SIZE] = strdup(command); // add the command to the history array
+    (*history_index)++; // increment the history index
 }
 
 bool checkHistory(char* tokens[], char* history[], int history_index) {
     // check for history command
-    if (strcmp(tokens[0], "history") == 0) {
+    if (strcmp(tokens[0], "history") == 0) { // if the user entered history
         // print previous commands with indices
         for (int i = 0; i < history_index; i++) {
-            cout << i+1 << ": " << history[i] << endl;
+            cout << i+1 << ": " << history[i] << endl; // print the command along with its index
         }
         return true;
     }
@@ -32,13 +32,13 @@ bool checkHistory(char* tokens[], char* history[], int history_index) {
     // execute command from history
     if (tokens[0][0] == 'h') { 
         // execute command with index from history
-        int index = atoi(tokens[0]+1);
-        if (index <= 0 || index > history_index) {
+        int index = atoi(tokens[0]+1); // convert the index from string to int
+        if (index <= 0 || index > history_index) { // check if the index is valid
             cout << "Invalid index." << endl;
             return true;
         }
-        char* command = strdup(history[(index-1) % HISTORY_SIZE]);
-        parseCommand(tokens, command);
+        char* command = strdup(history[(index-1) % HISTORY_SIZE]); // get the command from history
+        parseCommand(tokens, command); // parse the command
         free(command);
     }
 
